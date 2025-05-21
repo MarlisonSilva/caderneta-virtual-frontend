bcfa5163973d904c1d3a366007c310c761e18b57
manel <manelzaum@icloud.com>
2025-05-21 18:56:48 -0300

    feat: get real products from api.

--- caderneta-virtual/src/components/produtos/ProductTable.tsx
+++ caderneta-virtual/src/components/produtos/ProductTable.tsx
@@ -1,7 +1,21 @@
+import { Produto } from "@/types/product";
+import { fetchAPI } from "@/utils/connections";
 import Link from "next/link";
-import produtos from "../../data/produtos";
+import { useState, useEffect } from "react";
 
 export default function ProductTable() {
+  const [produtos, setProdutos] = useState<Produto[]>([]);
+
+  useEffect(() => {
+    fetchAPI("/products", "GET")
+      .then((data) => {
+        setProdutos(data);
+      })
+      .catch((err) => {
+        console.error("Erro ao buscar produtos:", err);
+      });
+  }, []);
+
   return (
     <div className="overflow-x-auto bg-white rounded-xl border border-[#ede9ff] shadow-md">
       <table className="w-full text-sm text-left text-[#1e1e2f] min-w-[600px]">
@@ -15,17 +29,15 @@ export default function ProductTable() {
           </tr>
         </thead>
         <tbody className="divide-y divide-[#f1efff]">
-          {produtos.map((p, i) => (
+          {produtos.map((p) => (
             <tr
-              key={i}
+              key={p.id}
               className="hover:bg-[#f7f6fc] transition-colors duration-150"
             >
-              <td className="px-6 py-4 font-medium text-[#5e5e7f]">
-                {p.nome}
-              </td>
-              <td className="px-6 py-4">{p.categoria}</td>
-              <td className="px-6 py-4">{p.cor}</td>
-              <td className="px-6 py-4">{p.capacidade}</td>
+              <td className="px-6 py-4 font-medium text-[#5e5e7f]">{p.name}</td>
+              <td className="px-6 py-4">{p.category.name}</td>
+              <td className="px-6 py-4">{p.color.name}</td>
+              <td className="px-6 py-4">{p.capacity}</td>
               <td className="px-6 py-4 text-right">
                 <Link
                   href={`/produtos/${p.id}`}
